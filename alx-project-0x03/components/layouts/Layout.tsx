c5bc39621520a4469e7fff62cc4e8e5c7e884cbc
Yared <yaredhabtamu08@gmail.com>
2025-07-27 22:31:24 +0300

    last commit

--- alx-project-0x03/components/layouts/Layout.tsx
+++ alx-project-0x03/components/layouts/Layout.tsx
@@ -1,11 +1,6 @@
-import { ReactNode } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import { LayoutProps } from "@/interface";
-
-interface LayoutProps {
-  children: ReactNode;
-}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
