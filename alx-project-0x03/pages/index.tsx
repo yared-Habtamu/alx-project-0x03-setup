5e5836d705eb9cc1c32676a6585d4e639de7a64a
Yared <yaredhabtamu08@gmail.com>
2025-07-27 22:38:59 +0300

    last commit

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -2,6 +2,17 @@ import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
 import { PageRouteProps } from "@/interface";
 
+import Button from "@/components/common/Button";
+import { useRouter } from "next/router";
+import { PageRouteProps } from "@/interface";
+
+export default function Home() {
+  const router = useRouter();
+
+  const routeToNextPage = ({ pageRoute }: PageRouteProps) => {
+    router.push(pageRoute, undefined, { shallow: false });
+  };
+
 interface PageRouteProps {
   pageRoute: string;
 }
